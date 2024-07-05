60fc30c7da93acef5bf4f573725bc76dc956b44b
2022dfff <1374490218@qq.com>
2024-07-05 18:20:14 +0800

    relogic the fig_plot part

--- tpf/fig_plot.py
+++ tpf/fig_plot.py
@@ -8,22 +8,39 @@
 from typing import Dict, List
 import networkx as nx
 import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 from interface import GraphData, GraphFeatures
 
 
-# The first part
+class PlotStorage:
+    def __init__(self, path: str):
+        sns.set_theme(style="whitegrid")
+        # TODO: adjust the sns style
+        self.path = path
+
+    def save_plot(self, name: str):
+        full_path = os.path.join(self.path, f"{name}.png")
+        os.makedirs(os.path.dirname(full_path), exist_ok=True)
+        plt.savefig(full_path)
+        plt.close()
+
+
 class GraphPlotter:
-    @classmethod
-    def plot_graph(cls, graph: nx.DiGraph, name: str, path: str):
-        # if name == "chain_xxx": use circular layout
-        # else: use spring layout
-        if "chain" in name:
-            plt.figure(figsize=(10, 10))
-            pos = nx.circular_layout(graph)
-        else:
-            plt.figure(figsize=(10, 10))
-            pos = nx.spring_layout(graph)
+    def __init__(self, path: str):
+        self.plot_storage = PlotStorage(path)
+        self.layout_mapping = {
+            "chain_0": nx.circular_layout,
+            "mesh_0": nx.spring_layout,
+            "pool_0": nx.spring_layout,
+            "star_0": nx.spring_layout,
+            "hierarchical_0": nx.spring_layout,
+        }
+
+    def plot_graph(self, graph: nx.DiGraph, name: str):
+        plt.figure(figsize=(10, 10))
+        layout_func = self.layout_mapping.get(name, nx.spring_layout)
+        pos = layout_func(graph)
         nx.draw(
             graph,
             pos,
@@ -33,31 +50,19 @@ class GraphPlotter:
             font_size=8,
         )
         plt.title(name)
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        plt.savefig(f"{path}{name}.png")
-        plt.close()
+        self.plot_storage.save_plot(name)
 
-    @classmethod
-    def plot_graphs(cls, graphs: List[GraphData], path: str):
+    def plot_graphs(self, graphs: List[GraphData]):
         # only plot the first graph for now (100 in total)
         for graph_data in graphs[:1]:
-            cls.plot_graph(graph_data.graph, graph_data.name, path)
+            self.plot_graph(graph_data.graph, graph_data.name)
 
 
-# The second part, plotting the graph features with scatter plots
 class GraphFeaturesPlotter:
-    centrality_mapping = {
-        "Average Degree Centrality": "degree_centrality",
-        "Average Betweenness Centrality": "betweenness_centrality",
-        "Average Closeness Centrality": "closeness_centrality",
-        # "Average Edge Betweenness Centrality": "edge_betweenness_centrality",
-        "Average Second Order Centrality": "second_order_centrality",
-        "Average Node Independence": "node_independence",
-        "Clustering Coefficient": "clustering_coefficient",
-    }
+    def __init__(self, path: str):
+        self.plot_storage = PlotStorage(path)
 
-    @classmethod
-    def plot_centrality_features(cls, feature: GraphFeatures, path: str):
+    def plot_centrality_features(self, feature: GraphFeatures):
         plt.figure(figsize=(12, 4))
         centrality_measures = [
             ("Degree Centrality", feature.degree_centrality),
@@ -74,21 +79,32 @@ class GraphFeaturesPlotter:
             plt.ylabel(title)
 
         plt.tight_layout()
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        plt.savefig(f"{path}{feature.name}_centrality.png")
-        plt.close()
+        self.plot_storage.save_plot(f"{feature.name}_centrality")
 
-    @classmethod
-    def plot_features_within_one_graph(
-        cls, features: List[GraphFeatures], path: str
-    ):
+    def plot_features_within_one_graph(self, features: List[GraphFeatures]):
         for feature_set in features[:1]:
-            cls.plot_centrality_features(feature_set, path)
+            self.plot_centrality_features(feature_set)
 
-    @classmethod
-    def plot_size_features_for_topologies(
-        cls, features: List[GraphFeatures], path: str
-    ):
+
+class TopoFeaturesPlotter:
+    def __init__(self, path: str):
+        self.plot_storage = PlotStorage(path)
+        self.path = path
+        self.centrality_mapping = {
+            "Average Degree Centrality": "degree_centrality",
+            "Average Betweenness Centrality": "betweenness_centrality",
+            "Average Closeness Centrality": "closeness_centrality",
+            "Average Second Order Centrality": "second_order_centrality",
+            "Average Node Independence": "node_independence",
+            "Clustering Coefficient": "clustering_coefficient",
+        }
+
+    def calculate_average_centrality(self, feature_set, centrality_type):
+        return sum(getattr(feature_set, centrality_type).values()) / len(
+            getattr(feature_set, centrality_type)
+        )
+
+    def plot_size_features_for_topologies(self, features: List[GraphFeatures]):
         name = features[0].name
         diameter = []
         for feature in features[0:100]:
@@ -97,38 +113,24 @@ class GraphFeaturesPlotter:
         plt.title(f"Diameter of {name}")
         plt.xlabel("Graph")
         plt.ylabel("Diameter")
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        plt.savefig(f"{path}{name}_diameter.png")
-        plt.close()
+        self.plot_storage.save_plot(f"{name}_diameter")
 
-    @classmethod
     def plot_centrality_features_for_topologies(
-        cls, features_dict: Dict[str, List[GraphFeatures]], path: str
+        self, features_dict: Dict[str, List[GraphFeatures]]
     ):
-        """
-        :param features_dict: a dictionary, the key is the topology name,
-            the value is the list of GraphFeatures.
-        :param path: the path to save the plots.
-        """
         plt.figure(figsize=(20, 10))
         colors = ["b", "g", "r", "c", "m", "y", "k"]
-
-        def calculate_average_centrality(feature_set, centrality_type):
-            return sum(getattr(feature_set, centrality_type).values()) / len(
-                getattr(feature_set, centrality_type)
-            )
-
         for i, centrality_title in enumerate(
-            cls.centrality_mapping.keys(), start=1
+            self.centrality_mapping.keys(), start=1
         ):
             plt.subplot(2, 3, i)
             for color, (topology_name, features) in zip(
                 colors, features_dict.items()
             ):
                 average_centralities = [
-                    calculate_average_centrality(
+                    self.calculate_average_centrality(
                         feature_set,
-                        cls.centrality_mapping[centrality_title],
+                        self.centrality_mapping[centrality_title],
                     )
                     for feature_set in features
                 ]
@@ -146,58 +148,24 @@ class GraphFeaturesPlotter:
             plt.legend()
 
         plt.tight_layout()
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        plt.savefig(f"{path}average_centrality_comparison.png")
-        plt.close()
+        self.plot_storage.save_plot("average_centrality_comparison")
 
-        # draw another plot with boxplot
-        plt.figure(figsize=(20, 10))
-        for i, centrality_title in enumerate(
-            cls.centrality_mapping.keys(), start=1
-        ):
-            plt.subplot(2, 3, i)
-            data = [
-                [
-                    calculate_average_centrality(
-                        feature_set,
-                        cls.centrality_mapping[centrality_title],
-                    )
-                    for feature_set in features
-                ]
-                for features in features_dict.values()
-            ]
-            plt.boxplot(data, labels=features_dict.keys())
-            plt.title(centrality_title)
-            plt.ylabel("Average Centrality")
-
-        plt.tight_layout()
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        plt.savefig(f"{path}average_centrality_boxplot.png")
-        plt.close()
-
-    @classmethod
     def plot_size_and_centrality_for_topologies(
-        cls, features_dict: Dict[str, List[GraphFeatures]], path: str
+        self, features_dict: Dict[str, List[GraphFeatures]]
     ):
         plt.figure(figsize=(20, 10))
         colors = ["b", "g", "r", "c", "m", "y", "k"]
-
-        def calculate_average_centrality(feature_set, centrality_type):
-            return sum(getattr(feature_set, centrality_type).values()) / len(
-                getattr(feature_set, centrality_type)
-            )
-
         for i, centrality_title in enumerate(
-            cls.centrality_mapping.keys(), start=1
+            self.centrality_mapping.keys(), start=1
         ):
             plt.subplot(2, 3, i)
             for color, (topology_name, features) in zip(
                 colors, features_dict.items()
             ):
                 average_centralities = [
-                    calculate_average_centrality(
+                    self.calculate_average_centrality(
                         feature_set,
-                        cls.centrality_mapping[centrality_title],
+                        self.centrality_mapping[centrality_title],
                     )
                     for feature_set in features
                 ]
@@ -215,6 +183,4 @@ class GraphFeaturesPlotter:
             plt.legend()
 
         plt.tight_layout()
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        plt.savefig(f"{path}diameter_centrality.png")
-        plt.close()
+        self.plot_storage.save_plot("diameter_centrality")
--- tpf/main.py
+++ tpf/main.py
@@ -2,8 +2,7 @@ import pickle
 from typing import List
 from agent_top import AgentGraphGenerator as Ag
 from evaluation import AgentFeatures as Af
-from fig_plot import GraphPlotter as Gp
-from fig_plot import GraphFeaturesPlotter as Gfp
+from fig_plot import GraphPlotter, GraphFeaturesPlotter, TopoFeaturesPlotter
 import os
 
 
@@ -30,32 +29,36 @@ generator_functions = {
     "stars": Ag.generate_stars,
 }
 
-# make "topo/" and "topo_fea/" defined paths
-topo_path = "stat/topo/"
-topo_fea_path = "stat/topo_fea/"
-topo_plot_path = "plot/topo_plots/"
-graph_fea_plot_path = "plot/graph_fea_plots/"
-topo_fea_plot_path = "plot/topo_fea_plots/"
+storge_paths = {
+    "topo_path": "stats/topo/",
+    "topo_fea_path": "stats/topo_features/",
+    "topo_plot_path": "plots/topo_plots/",
+    "graph_fea_plot_path": "plots/graph_features_plots/",
+    "topo_fea_plot_path": "plots/topo_features_plots/",
+}
 
 
 # generate topologies and save
 for topo in topologies:
     topo_data = generator_functions[topo](n_nodes_in_topologies, n_topologies)
-    save_file(topo_data, f"{topo_path}{topo}.pkl")
+    save_file(topo_data, f"{storge_paths['topo_path']}{topo}_test.pkl")
 
     # calculate features and save
     topo_features = Af.calculate_features(topo_data)
-    save_file(topo_features, f"{topo_fea_path}{topo}_features.pkl")
+    save_file(
+        topo_features, f"{storge_paths['topo_fea_path']}{topo}_features.pkl"
+    )
 
     # plot graphs and save
-    topo_plot = Gp.plot_graphs(topo_data, topo_plot_path)
+    Gp = GraphPlotter(storge_paths["topo_plot_path"])
+    topo_plot = Gp.plot_graphs(topo_data)
     print(f"{topo.capitalize()} Graphs Plotted.")
 
     # plot features and save
-    Gfp.plot_features_within_one_graph(topo_features, graph_fea_plot_path)
+    Gfp = GraphFeaturesPlotter(storge_paths["graph_fea_plot_path"])
+    Gfp.plot_features_within_one_graph(topo_features)
     # Gfp.plot_size_features_for_topologies(topo_features, topo_fea_plot_path)
     print(f"{topo.capitalize()} Features Plotted.")
-
     print(f"{topo.capitalize()} Done.")
     print("-------------------------------------------------")
     # pass
@@ -63,7 +66,8 @@ for topo in topologies:
 # create a: features_dict: Dict[str, List[GraphFeatures]]
 features_dict = {}
 for topo in topologies:
-    features = load_file(f"{topo_fea_path}{topo}_features.pkl")
+    features = load_file(f"{storge_paths['topo_fea_path']}{topo}_features.pkl")
     features_dict[topo] = features
-Gfp.plot_centrality_features_for_topologies(features_dict, topo_fea_plot_path)
-Gfp.plot_size_and_centrality_for_topologies(features_dict, topo_fea_plot_path)
+Tfp = TopoFeaturesPlotter(storge_paths["topo_fea_plot_path"])
+Tfp.plot_centrality_features_for_topologies(features_dict)
+Tfp.plot_size_and_centrality_for_topologies(features_dict)
